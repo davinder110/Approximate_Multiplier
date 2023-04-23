7e0810deb235d2d3ae2322c031395a12d8fef66c
Davinder Singh <davindersinghbhullar1@gmail.com>
2023-04-23 09:28:36 +0530

    save

--- New folder/FA.v
+++
@@ -1,8 +0,0 @@
-`timescale 1ns / 1ps
-
-module FA(input A,B,C,output Carry, Sum);
-wire Sel;
-assign Sel=A^B;
-assign Sum=Sel^C;
-assign Carry = Sel ? C :A ;
-endmodule
--- New folder/comp.v
+++
@@ -1,7 +0,0 @@
-`timescale 1ns / 1ps
-
-module comp(input A,B,C,D,output Carry, Sum);
-assign Sum= (~(A^B))|(~(C^D));
-assign Carry = (A | B) & (C | D);
-
-endmodule
